9fdfa3fdd1803731322fe4769237ce1e30f65036
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-20 12:59:54 -0600

    Create About component

---
+++ React-Business-Card/src/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <>
+            <h3 className="about_me">About</h3>
+            <p className="about--text">I am a frontend developer learning all things React. I have a particle interest in learning new skills. I do my best to keep up with the latest trend and to always learn something new each day.</p>
+        </>
+    )
+}
--- React-Business-Card/src/App.jsx
+++ React-Business-Card/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from "./profile.jsx"
+import About from "./About.jsx"
 
 export default function App() {
     return (
         <>
             <Profile />
+            <About />
         </>
     )
 }
