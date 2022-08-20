64d21fcf2545585ff084bca0b106ae096271ac99
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-20 13:08:15 -0600

    Create Interests component

--- React-Business-Card/src/App.jsx
+++ React-Business-Card/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Profile from "./profile.jsx"
 import About from "./About.jsx"
+import Interests from "./Interests.jsx"
 
 export default function App() {
     return (
         <>
             <Profile />
             <About />
+            <Interests />
         </>
     )
 }
---
+++ React-Business-Card/src/Interests.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Interests() {
+    return (
+        <>
+            <h3 className="interests">Interests</h3>
+            <p className="interests--text">Computer Expert. Gamer. Coffee Fanatic. Rubiks cube Lover.</p>
+        </>
+    )
+}
